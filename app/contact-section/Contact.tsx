a43b1a9e1d667d6853c76ce36bcd732974c71f75
Syed Nouman <noumanafzal4@gmail.com>
2023-11-20 13:19:00 +0530

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -46,10 +46,10 @@ const Contact = () => {
             <Link
               href="mailto:socialssea@gmail.com.ng?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
               target="_blank"
-              aria-label="Send me an email"
+              aria-label="Send us an email"
               className="mt-1 w-[147px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:w-[170px] lg:mt-4"
             >
-              <AnimatedBody text={"Send me an email"} className={""} />
+              <AnimatedBody text={"Send us an email"} className={""} />
             </Link>
           </div>
 
